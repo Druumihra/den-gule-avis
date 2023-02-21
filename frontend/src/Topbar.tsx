54cf073f4d16b9f75e87e96b1b4ee1ebcbea82aa
Mikkel <mtkongsted@gmail.com>
2023-02-21 13:36:37 +0100

    added topbar

---
+++ frontend/src/Topbar.tsx
@@ -0,0 +1,13 @@
+function Topbar() {
+
+    return (
+      <div className="Topbar">
+        <nav>
+          <h1>Den Scuffed Avis</h1>
+        </nav>
+      </div>
+    )
+  }
+  
+  export default Topbar
+  
