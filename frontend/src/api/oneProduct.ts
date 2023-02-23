2e25ae103561f2103723c79b4a15c4c1f164d760
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-02-23 10:16:23 +0100

    fix typo

--- frontend/src/api/oneProduct.ts
+++ frontend/src/api/oneProduct.ts
@@ -7,7 +7,7 @@ export default async function oneProduct(id: string): Promise<Product> {
 
         const product: Product = {
             id: "abcdefg",
-            title: "lækker suger klar på sjov",
+            title: "Lækker suger klar på sjov",
             image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.SXRY_iHx7osacNRG5OaoQwHaGj%26pid%3DApi&f=1&ipt=604696780da861d33ea4f4e97f0d698b4db02009d82703c3970c533a6c456b5a&ipo=images",
             description: "lorem ipsum dolor sit amet",
             comments,
