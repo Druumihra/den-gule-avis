d5c9b5d193a27c81573a69d1bac3457a07f4a4bf
Druumihra <teis.bindner@laerke.net>
2023-03-06 14:51:13 +0100

    Implemented Prisma deleteSession

--- auth/src/ORMDb.ts
+++ auth/src/ORMDb.ts
@@ -5,84 +5,101 @@ import { generateToken } from "./generateToken";
 
 const prisma = new PrismaClient();
 export class ORMDb implements Database {
-    public async addUser(user: User): Promise<boolean> {
-        try {
-            await prisma.users.create({
-            data: {
-                name: user.name,
-                password: user.password,
-                id: generateToken(64)
-            }
-        })
-        return true;
-    }   
-    catch {
-        return false;
+  public async addUser(user: User): Promise<boolean> {
+    try {
+      await prisma.users.create({
+        data: {
+          name: user.name,
+          password: user.password,
+          id: generateToken(64),
+        },
+      });
+      return true;
+    } catch {
+      return false;
     }
+  }
+  public async sessionFromToken(token: string): Promise<Session | null> {
+    try {
+      const res = await prisma.sessions.findFirst({
+        where: {
+          token: token,
+        },
+      });
+      return res;
+    } catch {
+      return null;
     }
-    public async sessionFromToken(token: string): Promise<Session | null> {
-        try {
-          const res = await prisma.sessions.findFirst({
-                where: {
-                    token: token
-                }
-            })
-            return res;
-        }
-        catch {
-            return null;
-        }
-    }
-    public async userFromId(id: string): Promise<(User & Identifiable) | null> {
-        try {
-            const res = await prisma.users.findUnique({
-                where: {
-                    id: id
-                }
-            })
+  }
+  public async userFromId(id: string): Promise<(User & Identifiable) | null> {
+    try {
+      const res = await prisma.users.findUnique({
+        where: {
+          id: id,
+        },
+      });
 
-            return res;
-        }
-        catch {
-            return null;
-        }
+      return res;
+    } catch {
+      return null;
     }
-    public async addSession(session: Session): Promise<boolean> {
-        try {
-            prisma.sessions.create({
-                data: {
-                   id: generateToken(64),
-                   token: session.token,
-                   userId: session.userId
-                }
-            })
-            return true;
-        }
-        catch {
-            return false;
-        }
+  }
+  public async addSession(session: Session): Promise<boolean> {
+    try {
+      prisma.sessions.create({
+        data: {
+          id: generateToken(64),
+          token: session.token,
+          userId: session.userId,
+        },
+      });
+      return true;
+    } catch {
+      return false;
     }
-    public async userFromName(name: string): Promise<(User & Identifiable) | null> {
-        try {
-            const res = await prisma.users.findFirst({
-                where: {
-                    name: {
-                        equals: name
-                    }
-                },
-                select: {
-                    name: true,
-                    password: true,
-                    id: true
-                }
-            })
-            return res;
-        }
-        catch {
-            return null;
-        }
+  }
+  public async userFromName(
+    name: string,
+  ): Promise<(User & Identifiable) | null> {
+    try {
+      const res = await prisma.users.findFirst({
+        where: {
+          name: {
+            equals: name,
+          },
+        },
+        select: {
+          name: true,
+          password: true,
+          id: true,
+        },
+      });
+      return res;
+    } catch {
+      return null;
     }
-    public async deleteSession(session: Session): Promise<boolean> {
-        throw new Error("Not yet implemented");
+  }
+  public async deleteSession(session: Session): Promise<boolean> {
+    try {
+      const idResult: Identifiable | null = await prisma.sessions.findFirst({
+        where: {
+          token: session.token,
+        },
+        select: {
+          id: true,
+        },
+      });
+      if (!idResult) {
+        return false;
+      }
+      await prisma.sessions.delete({
+        where: {
+          id: idResult.id,
+        },
+      });
+      return true;
+    } catch {
+      return false;
     }
+  }
 }
