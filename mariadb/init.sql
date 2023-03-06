7c4f3127e27a90c4d87702b4fcb6419da51bb58b
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-03-06 15:09:04 +0100

    text for ids

--- mariadb/init.sql
+++ mariadb/init.sql
@@ -9,18 +9,18 @@ DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS products;
 
 CREATE TABLE products(
-id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-title TEXT NOT NULL,
-description TEXT NOT NULL,
-image TEXT NOT NULL
+	id TEXT PRIMARY KEY NOT NULL,
+	title TEXT NOT NULL,
+	description TEXT NOT NULL,
+	image TEXT NOT NULL
 );
 
 CREATE TABLE comments(
-id INT PRIMARY KEY AUTO_INCREMENT,
-text TEXT NOT NULL,
-user_id TEXT NOT NULL,
-product_id INT NOT NULL,
-FOREIGN KEY (product_id) REFERENCES products(id)
+	id TEXT PRIMARY KEY NOT NULL,
+	text TEXT NOT NULL,
+	user_id TEXT NOT NULL,
+	product_id TEXT NOT NULL,
+	FOREIGN KEY (product_id) REFERENCES products(id)
 );
 
 INSERT INTO products (title, description, image) VALUES ("flot fyr 1", "mon han kan lide krabbesproget?", "https://tpho.dk/images/me.png");
